95faffe7cf41562e61556b4cdb6219ac87dda2c9
Luis Felipe Cerero Garcia <lfcerero@misena.edu.co>
2021-01-06 09:23:11 -0500

    Creacion de proyecto con react para integración con vtex io, que es como funciona entre otras cosas, como el cambio de idioma de la plataforma.

--- react/Countdown.tsx
+++ react/Countdown.tsx
@@ -2,15 +2,18 @@ import React, { useState } from 'react'
 import { TimeSplit } from './typings/global'
 import { tick, getTwoDaysFromNow } from './utils/time'
 import { useCssHandles } from 'vtex.css-handles'
+import { FormattedMessage } from 'react-intl'
 
 const DEFAULT_TARGET_DATE = getTwoDaysFromNow()
-const CSS_HANDLES = ['countdown']
+const CSS_HANDLES = ['container', 'countdown', 'title']
 
 interface CountdownProps {
+  title: string,
   targetDate: string
 }
 
 const Countdown: StorefrontFunctionComponent<CountdownProps> = ({
+  title,
   targetDate = DEFAULT_TARGET_DATE,
 }) => {
 
@@ -20,25 +23,34 @@ const Countdown: StorefrontFunctionComponent<CountdownProps> = ({
     seconds: '00'
   })
 
+  const titleText = title || <FormattedMessage id="countdown.title" />
   const handles = useCssHandles(CSS_HANDLES)
 
   tick(targetDate, setTime)
 
   return (
+    <div className={`${handles.container} t-heading-2 fw3 w-100 c-muted-1`}>
+      <div className={`${handles.title} db tc`}>{titleText}</div>
       <div className={`${handles.countdown} c-muted-1 db tc`}>
-        {/* <h1>
-          {targetDate}
-        </h1> */}
-          <h1>{ `${timeRemaining.hours}:${timeRemaining.minutes}:${timeRemaining.seconds}` }</h1>
-    </div>
+          {/* <h1>
+            {targetDate}
+          </h1> */}
+            <h1>{ `${timeRemaining.hours}:${timeRemaining.minutes}:${timeRemaining.seconds}` }</h1>
+      </div>
+    </div>     
   )
 }
 
 Countdown.schema = {
-  title: 'editor.countdown.title',
-  description: 'editor.countdown.description',
+  title: 'TEST 2',
+  description: 'aloha',
   type: 'object',
   properties: {
+    title: {
+      title: 'Este es el titulo de felipe ;)',
+      type: 'string',
+      default: null,
+    },
     targetDate:{
       title: 'Final Date for Felipe Garcia',
       description: 'Final date used in the countdown',
